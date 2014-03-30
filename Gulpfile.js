69f707b0468f79e467b4a574b3229c8f1e769d06
Hendrixer <willscottmoss@gmail.com>
2014-03-30 00:54:24 -0700

    (spec) create specs

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,28 @@
 var gulp        = require('gulp'),
     server      = require('tiny-lr')(),
     refresh     = require('gulp-livereload'),
+    mocha       = require('gulp-mocha'),
     app         = require('./server/server.js'),
     lrPort      = 35729;
 
 // collect paths
-var paths = ['./client/src/**/*.js', './client/src/**/*.html', '!client/lib/'];
+var paths = {
+  client: {
+    app: ['./client/src/**/*.js', './client/src/**/*.html', '!client/lib/']
+  },
+  server: {
+    specs: {
+      cards: {
+        unit: './server/cards/specs/*.js'
+      }
+    }
+  }
+};
+
+gulp.task('test', function(){
+  gulp.src(paths.server.specs.cards.unit)
+    .pipe(mocha({reporter: 'spec'}));
+});
 
 // serve express server
 gulp.task('serve', function(){
@@ -15,7 +32,7 @@ gulp.task('serve', function(){
 
 // setup task to reload server on any file change in client path
 gulp.task('client', function(){
-  gulp.src(paths)
+  gulp.src(paths.client.app)
     .pipe(refresh(server));
 });
 
@@ -24,7 +41,7 @@ gulp.task('default', ['serve'], function(){
   server.listen(lrPort, function(err){
     if(err) {return console.error(err);}
 
-    gulp.watch(paths, function(){
+    gulp.watch(paths.client.app, function(){
       gulp.run('client');
     });
   });
---
+++ server/cards/specs/cardSpec.js
@@ -0,0 +1,39 @@
+process.NODE_ENV = 'test';
+
+var expect = require('expect.js'),
+    Card   = require('../cardMaker.js')().card;
+
+describe('Card constructor', function(){
+  var card;
+  var tweet = {
+    text: "I'm tweeting",
+    user: {
+      profile_img_url: 'http://yo.com',
+      screen_name: 'scotups'
+    },
+    tweetCard: true
+  };
+
+  before(function (done){
+    card = new Card();
+    done();
+  });
+
+  describe('New card', function(){
+    it('should be a new instance of Card', function(done){
+      expect(card).to.be.an(Object);
+      expect(card.constructor).to.eql(Card);
+      done();
+    });
+  });
+  describe('Format new card', function(){
+    it('should format Tweet', function(done){
+      card.format(tweet);
+      expect(card.card_type).to.be('Social');
+      expect(card.origin).to.be('Twitter');
+      expect(card.user_name).to.be('@scotups');
+      expect(card.url).to.be('https://twitter.com/scotups');
+      done();
+    });
+  });
+});
